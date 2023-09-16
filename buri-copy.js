14143279decc17b7b2f06b60f4bf584a5cd1ded5
zaki <zaki.hmkc@gmail.com>
2023-09-16 15:54:16 +0900

    update: addEventListenerを使ったイベント登録とボタン押下の処理(クリップボード処理はまだ失敗)

--- buri-copy.js
+++ buri-copy.js
@@ -1,12 +1,17 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     console.log(request);
-    navigator.clipboard.writeText(request);
+    // navigator.clipboard.writeText(request);
 
     resource_id = '/' + request.split('/').slice(5,13).join('/');
     console.log(resource_id);
-    navigator.clipboard.writeText(resource_id);
+    // navigator.clipboard.writeText(resource_id);
+    // ここでコピーしようとすると以下のエラーが出る
+    //  Uncaught (in promise) DOMException: Document is not focused.
 
-    // sendResponse({
-    //     value: request,
-    // });
+    sendResponse({
+        value: request,
+        id: resource_id
+    });
+    // responseは返さないとエラーが出る
+    // Unchecked runtime.lastError: The message port closed before a response was received.
 });
--- buri-popup.js
+++ buri-popup.js
@@ -9,3 +9,37 @@ function sample_function() {
 // formのonlick()でなくスクリプト側からリスナーを設定する
 // https://teratail.com/questions/116400
 document.getElementById('sample_id').addEventListener('click', {handleEvent: sample_function, param: "aiueo"});
+
+// window.onload = function() {
+//     // console.log("lonload");
+//     // alert("aaaaa") # OK
+//     let param = this.param;
+//     // chrome.tabs.query( {active: true, currentWindow: true}, function(tabs) {
+//     //     // chrome.tabs.sendMessage(tabs[0].id, tabs[0].url, function(response) {
+//     //     //     console.log("sendmessage")
+//     //     // });
+//     //     chrome.tabs.sendMessage(tabs[0].id, param);
+//     //     // chrome.tabs.sendMessage(tabs[0].id, tabs[0].url);
+//     // });
+
+//     // chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, tabs => {
+//     //     chrome.tabs.sendMessage(tabs[0].id, location.href, function(response) {
+//     //         // console.log(response);
+//     //     });
+//     // });
+// };
+
+window.addEventListener('load',()=>{
+    // alert("aaaaa")
+    // console.log("bbbb")
+
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, tabs => {
+        chrome.tabs.sendMessage(tabs[0].id, tabs[0].url, function(response) {
+            console.log(response);
+            // alert(response.id)
+            // navigator.clipboard.writeText(response.id);
+            // こっちでも同じエラー
+            // Uncaught (in promise) DOMException: Document is not focused.
+        });
+    });
+});
