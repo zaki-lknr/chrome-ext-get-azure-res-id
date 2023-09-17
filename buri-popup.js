540f832683aa8e22005bebd9ee00542a9e7dd051
zaki <zaki.hmkc@gmail.com>
2023-09-17 14:00:11 +0900

    add: copyボタンの追加

--- buri-popup.js
+++ buri-popup.js
@@ -1,21 +1,12 @@
-// function sample_function() {
-//     console.log("sample function");
-//     let param = this.param;
-//     chrome.tabs.query( {active: true, currentWindow: true}, function(tabs) {
-//         // chrome.tabs.sendMessage(tabs[0].id, param);
-//         // chrome.tabs.sendMessage(tabs[0].id, tabs[0].url, function(response) {
-//         //     console.log("sample_func: " + response);
-//         //     // alert(response.id)
-//         //     navigator.clipboard.writeText(response.id);
-//         //     // Uncaught (in promise) DOMException: Document is not focused.
-//         // });
-
-//     });
-// }
+function press_copy_button() {
+    console.log("sample function");
+    resource_id = document.getElementById('resource_id').value;
+    navigator.clipboard.writeText(resource_id);
+}
 
 // // formのonlick()でなくスクリプト側からリスナーを設定する
 // // https://teratail.com/questions/116400
-// document.getElementById('sample_id').addEventListener('click', {handleEvent: sample_function, param: "aiueo"});
+document.getElementById('copy').addEventListener('click', {handleEvent: press_copy_button});
 
 // window.onload = function() {
 //     // console.log("lonload");
